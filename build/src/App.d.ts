23f74c59994c669fd01eaffad07eea1d84d3f403
Vivian Erbenich <vivian@erbenich.eu>
2022-04-03 23:57:48 +0200

    removes build folder

--- build/src/App.d.ts
+++
@@ -1,8 +0,0 @@
-import Controller from './interfaces/Controller';
-import XboxController from './controller/XBoxController';
-declare class App {
-    controller: Controller | XboxController;
-    constructor();
-    run(): Promise<void>;
-}
-export default App;
--- build/src/interfaces/IStickMotionEvent.d.ts
+++
@@ -1,4 +0,0 @@
-export default interface StickMotionEvent {
-    x: number;
-    y: number;
-}
